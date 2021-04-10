962f2a0149b8752b07fce9bc37a328f186c5dac5
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-04-10 17:44:48 -0500

    Movie Search App
    Using API and React, I created this web app to search for movies.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchMovies from "./SearchMovies";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Movie Search</h1>
+        <SearchMovies/>
+              <footer>
+       This web app was created with the help of Scrimba. All code belongs to them 
+       and their creators. API provided by <a href='https://www.themoviedb.org/'>TMDB</a>.
+      </footer>
+      </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
